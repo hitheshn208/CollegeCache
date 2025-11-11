544988c3cb978eb6986cb88bd82056ae991c69ef
hitheshn208 <hitheshn2006@gmail.com>
2025-11-11 23:27:45 +0530

    Refactor precedence handling in infix to postfix conversion and improve error messages

--- 3rd Sem/Data Structures in C/infix_to_postfix.c
+++ 3rd Sem/Data Structures in C/infix_to_postfix.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #define MAX 20
 
-typedef enum {lparen, rparen, add, sub, divide, mul, mod, operand, eos, operator} precedence;
+typedef enum {rparen, operand, eos, operator} precedence;
 char stack[MAX];
 int top = -1;
 char expr[MAX];
@@ -11,7 +11,7 @@ char pop()
 {
     if(top == -1)
     {
-        printf("the Stack is full!!\n");
+        printf("\nThe Stack is empty!!\n");
         exit(1);
     }
     return stack[top--];
@@ -22,6 +22,20 @@ void push(char symbol)
     stack[++top] = symbol;
 }
 
+int precedenceChecker(char op)
+{
+    switch(op)
+    {
+        case '(':
+        case ')': return 3;
+        case '*':
+        case '/':
+        case '%': return 2;
+        case '+':
+        case '-': return 1;
+    }
+}
+
 precedence getToken(char *symbol, int *n)
 {
     *symbol = expr[(*n)++];
@@ -29,11 +43,12 @@ precedence getToken(char *symbol, int *n)
     switch(*symbol)
     {
         case ')' : return rparen;
-        case '(' : return lparen;
-        case '+' : return add;
-        case '-' : return sub;
-        case '*' : return mul;
-        case '/' : return divide;
+        case '(' : 
+        case '+' : 
+        case '-' : 
+        case '*' : 
+        case '%' :
+        case '/' : return operator;
         case '\0' : return eos;
         default : return operand;
     }
@@ -61,7 +76,16 @@ void postfix()
         }
         else
         {
+            /*If the precedence of the operator in stack is greater than the operator to be pushed then, pop the stack until the operator in stack has less precedence or the stack is empty*/
+            while( precedenceChecker(symbol) <= precedenceChecker(stack[top]) && stack[top]!='(' )
+            {
+                printf("%c", pop());
+
+                if(top == -1)
+                    break;
+            }
             push(symbol);
+
         }
 
         token = getToken(&symbol, &n);
